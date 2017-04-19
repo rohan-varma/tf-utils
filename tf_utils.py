8c89ba8b4093311fac233afa977ec25c7c0186ec
Rohan Varma <rvarm1@gmail.com>
2017-04-18 20:30:39 -0700

    tensorflow utils

---
+++ tf_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+def weight_variable(shape, name = None):
+    pass
+
+def bias_variable(shape, name = None):
+    pass
+
+def conv2d(x, W, strides = [1,1,1,1], padding = 'SAME', name = None):
+    pass
+
+def max_pool_2x2(x, name = None):
+    pass
+
+def conv_layer():
+    pass
+
+def fc_layer():
+
+def get_acc():
+    pass
+
+def create_summary(names = []_):
+    pass
+
